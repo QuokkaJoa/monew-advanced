293e30cec0685fdb8de4b3c19ad961b4b6e262af
KimSeeun <9035anny@naver.com>
2025-05-31 16:56:41 +0900

    feat(S0MT-14): updateNickname 메서드 추가

--- src/main/java/com/part2/monew/service/UserService.java
+++ src/main/java/com/part2/monew/service/UserService.java
@@ -1,10 +1,12 @@
 package com.part2.monew.service;
 
 import com.part2.monew.dto.request.UserCreateRequest;
+import com.part2.monew.dto.request.UserUpdateRequest;
 import com.part2.monew.dto.response.UserResponse;
 import com.part2.monew.entity.User;
 import com.part2.monew.mapper.UserMapper;
 import com.part2.monew.repository.UserRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,4 +26,15 @@ public class UserService {
         return userMapper.toResponse(user);
     }
 
+    public UserResponse updateNickname(UUID userId, UUID requestUserId, UserUpdateRequest request) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!user.getId().equals(requestUserId)){
+            throw new RuntimeException("You are not alloawd to modify this user");
+        }
+        userMapper.updateNickname(request, user);
+        userRepository.save(user);
+        return userMapper.toResponse(user);
+    }
 }
