82cc93d37336d52871b8ebae32888ea1dc05473d
KimSeeun <9035anny@naver.com>
2025-05-31 16:58:24 +0900

    feat(S0MT-14): 닉네임 수정 DTO 구현

---
+++ src/main/java/com/part2/monew/dto/request/UserUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.part2.monew.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserUpdateRequest {
+
+  @NotBlank(message ="닉네임은 필수입니다.")
+  @Size(min = 1, max = 20, message = "닉네임은 1자 이상 20자 이하로 입력해주세요.")
+  private String nickname;
+}
+
