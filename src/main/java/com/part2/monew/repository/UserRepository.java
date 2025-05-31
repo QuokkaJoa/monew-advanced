95b6e589b7160b66e55dad8e51e9d79c4316de36
KimSeeun <9035anny@naver.com>
2025-05-31 21:22:32 +0900

    feat(S0MT-15): findByIdAndActiveTrue() 추가

--- src/main/java/com/part2/monew/repository/UserRepository.java
+++ src/main/java/com/part2/monew/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.part2.monew.repository;
 
 import com.part2.monew.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Boolean existsByEmail(String email);
+    Optional<User> findByIdAndActiveTrue(UUID id);
 }
