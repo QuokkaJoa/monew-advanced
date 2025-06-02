f0cfbfc558d181a3a5473815f07df2d9909febba
KimSeeun <9035anny@naver.com>
2025-06-02 23:07:46 +0900

    feat(S0MT-17): UserNotFoundException 예외 처리

---
+++ src/main/java/com/part2/monew/global/exception/user/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.part2.monew.global.exception.user;
+
+import com.part2.monew.global.exception.BusinessException;
+import com.part2.monew.global.exception.ErrorCode;
+
+public class UserNotFoundException extends BusinessException {
+
+  public UserNotFoundException(String detailMessage) {
+    super(ErrorCode.USER_NOT_FOUND, detailMessage);
+  }
+}
