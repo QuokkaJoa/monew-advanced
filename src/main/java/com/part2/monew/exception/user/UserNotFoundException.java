38595f935e5ea9b5d2083e0d833ee243bdcfe491
KimSeeun <9035anny@naver.com>
2025-06-02 16:48:28 +0900

    refactor(S0MT-17): 사용자 조회 실패 커스텀 예외로 처리

--- src/main/java/com/part2/monew/exception/user/UserNotFoundException.java
+++ src/main/java/com/part2/monew/exception/user/UserNotFoundException.java
@@ -1,9 +1,20 @@
 package com.part2.monew.exception.user;
 
-public class UserNotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class UserNotFoundException extends UserException {
 
   public UserNotFoundException() {
       super("해당 사용자를 찾을 수 없습니다.");
   }
 
+  @Override
+  public String getErrorCode() {
+    return "USER_NOT_FOUND";
+  }
+
+  @Override
+  public HttpStatus getStatus() {
+    return HttpStatus.NOT_FOUND;
+  }
 }
