2192d8ae20e008cbecaacd268436a80fb91be781
KimSeeun <9035anny@naver.com>
2025-06-02 16:46:52 +0900

    refactor(S0MT-17): 이메일 중복 커스텀 예외로 처리

--- src/main/java/com/part2/monew/exception/user/EmailDuplicateException.java
+++ src/main/java/com/part2/monew/exception/user/EmailDuplicateException.java
@@ -1,9 +1,20 @@
 package com.part2.monew.exception.user;
 
-public class EmailDuplicateException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class EmailDuplicateException extends UserException {
 
   public EmailDuplicateException() {
     super("이미 사용 중인 이메일입니다.");
   }
 
+  @Override
+  public String getErrorCode() {
+    return "EMAIL_DUPLICATED";
+  }
+
+  @Override
+  public HttpStatus getStatus() {
+    return HttpStatus.CONFLICT;
+  }
 }
