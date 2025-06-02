1e9570c3929753f9d92adf29b2a92b4507c65f5e
KimSeeun <9035anny@naver.com>
2025-06-02 13:39:39 +0900

    feat(S0MT-17): EmailDuplicateException 예외 클래스 추가

---
+++ src/main/java/com/part2/monew/exception/EmailDuplicateException.java
@@ -0,0 +1,9 @@
+package com.part2.monew.exception;
+
+public class EmailDuplicateException extends RuntimeException {
+
+  public EmailDuplicateException() {
+    super("이미 사용 중인 이메일입니다.");
+  }
+
+}
