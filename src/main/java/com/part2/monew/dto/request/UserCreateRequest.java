885d304e8663561b8510f1da3f892b95bfcb387a
KimSeeun <9035anny@naver.com>
2025-06-02 09:34:57 +0900

    chore(S0MT-15): 유효성 검사 조건 추가

--- src/main/java/com/part2/monew/dto/request/UserCreateRequest.java
+++ src/main/java/com/part2/monew/dto/request/UserCreateRequest.java
@@ -2,16 +2,20 @@ package com.part2.monew.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
-        @Email(message = "이메일 형식이 아닙니다")
-        @NotBlank(message = "이메일은 필수입니다")
+
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "이메일 형식이 아닙니다.")
         String email,
 
-        @NotBlank(message = "닉네임은 필수입니다")
+        @NotBlank(message = "닉네임은 필수입니다.")
+        @Size(min = 1, max = 20, message = "닉네임은 1자 이상 20자 이하로 입력해주세요.")
         String nickname,
 
-        @NotBlank(message = "비밀번호는 필수입니다")
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Size(min = 6, max = 20, message = "비밀번호는 6자 이상 20자 이하로 입력해주세요.")
         String password
 ) {
 }
