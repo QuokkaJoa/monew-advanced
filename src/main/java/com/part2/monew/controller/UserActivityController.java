91283f2ba9cd7a344a0d8449d698d2eb72555bb5
KimSeeun <9035anny@naver.com>
2025-06-05 01:51:48 +0900

    feat(S0MT-29): 사용자 활동 내역 controller 구현

---
+++ src/main/java/com/part2/monew/controller/UserActivityController.java
@@ -0,0 +1,23 @@
+package com.part2.monew.controller;
+
+import com.part2.monew.dto.response.UserActivityResponse;
+import com.part2.monew.service.UserActivityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user-activities")
+public class UserActivityController {
+
+  private final UserActivityService userActivityService;
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<UserActivityResponse> getUserActivity(@PathVariable UUID userId) {
+    UserActivityResponse response = userActivityService.getUserActivity(userId);
+    return ResponseEntity.ok(response);
+  }
+}
